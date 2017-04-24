c975f2a99a811fda254ff5ab244fe90a5e553c07
Adila <a_mebrek@esi.dz>
2017-04-24 18:17:17 +0200

    Add files via upload

---
+++ EAFOG/delay_calculation.m
@@ -0,0 +1,7 @@
+function [ output_args ] = delay_calculation( input_args )
+%DELAY_CALCULATION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ EAFOG/energy_consmuption.m
@@ -0,0 +1,7 @@
+function [ output_args ] = energy_consmuption( input_args )
+%ENERGY_CONSMUPTION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
